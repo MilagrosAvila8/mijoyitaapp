003acdd2ce58bbd6f6d06aa921157fb391026cb1
lourdes correa <lourdesma.correa@gmail.com>
2024-09-03 17:47:18 -0500

    migracion de cart y line items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_03_175117) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_03_211652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_175117) do
     t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "line_items", force: :cascade do |t|
@@ -55,7 +57,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_175117) do
     t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -80,5 +84,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_175117) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
   add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
